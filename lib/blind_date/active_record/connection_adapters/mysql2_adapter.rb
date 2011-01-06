d723f2f7143027b94551433c5614cbc2434ec911
Ari Epstein <ate2@cornell.edu>
2011-01-06 14:38:24 -0500

    Mysql2 support.

--- lib/blind_date/active_record/connection_adapters/mysql2_adapter.rb
+++ lib/blind_date/active_record/connection_adapters/mysql2_adapter.rb
@@ -1,2 +1,25 @@
-require 'blind_date/active_record/connection_adapters/mysql_adapter.rb'
+module BlindDate
+  module ActiveRecord
+    module ConnectionAdapters
+      module Mysql2Adapter
+        module ClassMethods
+          def date_add_sql( style, sql, interval, unit, operator )
+            "( #{sql} #{operator} INTERVAL #{interval} #{unit} )"
+          end
+        end
+
+        module InstanceMethods
+
+        end
+
+        def self.included(receiver)
+          receiver.extend         ClassMethods
+          receiver.send :include, InstanceMethods
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::ConnectionAdapters::Mysql2Adapter.send :include, BlindDate::ActiveRecord::ConnectionAdapters::Mysql2Adapter
 
